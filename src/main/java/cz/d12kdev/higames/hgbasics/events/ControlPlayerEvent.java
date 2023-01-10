08008c49e8d1a47760ba7cdbfb3b5b8f3785b056
d12kdev <d12k@outlook.cz>
2023-01-10 16:35:11 +0100

    Event Class Update

--- src/main/java/cz/d12kdev/higames/hgbasics/events/ControlPlayerEvent.java
+++ src/main/java/cz/d12kdev/higames/hgbasics/events/ControlPlayerEvent.java
@@ -1,15 +1,33 @@
 package cz.d12kdev.higames.hgbasics.events;
 
+import de.tr7zw.nbtapi.NBTItem;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import static cz.d12kdev.higames.hgbasics.PlugConf.prefix;
 
 public class ControlPlayerEvent{
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand(); // gets the item in the player's main hand
-        player.sendMessage("You used a " + item.getType() + "!");
+        NBTItem used = new NBTItem(item);
+        String targetname = used.getString("plr");
+        String action = used.getString("act");
+        Player target = Bukkit.getPlayerExact(targetname);
+        if(target == null){
+            player.sendMessage(prefix + " §cTarget Player not found");
+            player.sendActionBar("§c§lError! §r Look into chat");
+        } else {
+            if(action == "xforward"){
+                Vector add = new Vector(1,0,0);
+                Vector direction = target.getLocation().getDirection().add(add);
+                target.setVelocity(direction);
+            }
+        }
     }
 }
